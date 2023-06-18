52b8ec04bf2a357e0ee10114b01b4f14d24e8147
MiracleNearby <101981875+MiracleNear@users.noreply.github.com>
2023-06-18 22:41:21 +0300

    hello

--- main.py
+++ main.py
@@ -11,6 +11,6 @@ def printhi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    printhi('PyCharm')
+    printhi('PyCharm    ')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
